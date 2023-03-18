9bf9fc2a2b06612e59fc3009656c797f2d119e57
fartun <fartunwario34@gmail.com>
2023-03-18 17:57:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+    if (winRecord) {
+      return winRecord.year;
+    } else {
+      return undefined;
+    }
+  }
